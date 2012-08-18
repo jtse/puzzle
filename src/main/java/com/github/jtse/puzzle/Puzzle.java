8b1d1485fc89dfbd8e48e6c61d6b154d1629ff7c
Jim Tse <github@jimtse.com>
2012-08-18 17:18:48 -0400

    move renderImage to Textures

--- src/main/java/com/github/jtse/puzzle/Puzzle.java
+++ src/main/java/com/github/jtse/puzzle/Puzzle.java
@@ -21,11 +21,9 @@ import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glBlendFunc;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
 import static org.lwjgl.opengl.GL11.glMatrixMode;
 import static org.lwjgl.opengl.GL11.glOrtho;
@@ -48,6 +46,7 @@ import org.slf4j.LoggerFactory;
 
 import com.github.jtse.puzzle.ogl.ConfigureSceneModule;
 import com.github.jtse.puzzle.ogl.Region;
+import com.github.jtse.puzzle.ogl.Textures;
 import com.github.jtse.puzzle.physics.Displacement;
 import com.github.jtse.puzzle.physics.PhysicsModule;
 import com.github.jtse.puzzle.ui.DeltaMouseEventFilter;
@@ -210,7 +209,7 @@ public class Puzzle {
             s.append(",");
           }
 
-          renderImage(textures[i], x, y);
+          Textures.renderImage(textures[i], x, y);
         }
         log.info(s.toString(), textures.length);
 
@@ -225,53 +224,6 @@ public class Puzzle {
       return;
     }
     Display.destroy();
-  }
-
-  /**
-   * Renders a single image
-   *
-   * @param texture
-   * @param x
-   * @param y
-   */
-  private static void renderImage(Texture texture, int x, int y) {
-    // Color.white.bind();
-    // texture.bind(); // or GL11.glBind(texture.getTextureID());
-    //GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    GL11.glBindTexture(GL_TEXTURE_2D, texture.getTextureID());
-
-    // GL11.glTexEnvf(GL11.GL_TEXTURE_ENV, GL11.GL_TEXTURE_ENV_MODE,
-    // GL11.GL_MODULATE );
-
-    // when texture area is small, bilinear filter the closest mipmap
-
-    // GL11.glTexParameterf( GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
-    // GL11.GL_LINEAR_MIPMAP_NEAREST );
-    // when texture area is large, bilinear filter the first mipmap
-    // GL11.glTexParameterf( GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER,
-    // GL11.GL_LINEAR );
-
-    // GL11.glTexParameterf( GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S,
-    // GL11.GL_CLAMP );
-    // GL11.glTexParameterf( GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T,
-    // GL11.GL_CLAMP );
-
-    glBegin(GL11.GL_QUADS);
-
-    GL11.glTexCoord2d(0.0d, 0.0d);
-    GL11.glVertex2d(x, y);
-
-    // It's 0.99 instead of 1.0 because 1.0 creates edge artifacts
-    GL11.glTexCoord2d(0.99d, 0.0d);
-    GL11.glVertex2d(x + texture.getTextureWidth(), y);
-
-    GL11.glTexCoord2d(0.99d, 0.99d);
-    GL11.glVertex2d(x + texture.getTextureWidth(), y + texture.getTextureHeight());
-
-    GL11.glTexCoord2d(0.0d, 0.99d);
-    GL11.glVertex2d(x, y + texture.getTextureHeight());
-
-    glEnd();
   }
 
   private static void setRegionPositions(Region[] regions, Point[] points) {
